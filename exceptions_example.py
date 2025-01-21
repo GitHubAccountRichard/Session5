463e0f7d710c384dc264f7119ca9df9577e8e7e3
Richard Glatter <rglatter.ieu2023@student.ie.edu>
2025-01-21 17:50:49 +0100

    Added an example

--- exceptions_example.py
+++ exceptions_example.py
@@ -1,13 +1,14 @@
 name = input("What is your name? ")
 print("Hello",  name)
 age = input("How old are you? ")
-#another way to format print is via f-strings
 try:
     print(f"{name}, you were born in {2024 - int(age)}")
-    division = int(age) / 0
 except ZeroDivisionError: #if you run the code you can see the type of error
     print("Age must be a valid numeric value")
     print(f"the value that you typed was {age}")
 except: #all other types of exceptions
     print("Something else went wrong")
-print("thanks for playing")
+else: #in case no exception happened
+    print("Thanks for being a good sport and not trying to crash the app")
+finally: #this runs in the end no matter what
+    print("thanks for playing")
